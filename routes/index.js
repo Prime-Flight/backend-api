21cd1315a6c8df3c990728a82ea7c0ba840600ea
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-11-23 01:54:16 +0800

    feat: api documentation for auth and create the morgan logging

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const router = express.Router(); 
 const controller = require('../controllers');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const apiDocument = YAML.load('./docs/api-docs.yaml');
 
+// for the checker for hello world
 router.get('/', controller.hello);
+// for the Swagger API Documentation
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(apiDocument));
 
 module.exports = router;
