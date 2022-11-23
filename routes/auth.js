777668a52099a59eeffdf9be65188e37a35661e4
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-11-23 11:08:29 +0800

    add routes template

---
+++ routes/auth.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const router = express.Router(); 
+const controller = require('../controllers');
+
+router.post('/register', controller.auth.register);
+router.post('/login', controller.auth.login);
+router.get('/login/google', controller.auth.google);
+router.get('/whoami', controller.auth.google);
+router.get('/forgot-password', controller.auth.viewForgotPassword);
+router.post('/forgot-password', controller.auth.forgotPassword);
+router.post('/reset-password', controller.auth.resetPassword);
+router.post('/verify-user', controller.auth.verifyUser);
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,13 @@ const controller = require('../controllers');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const apiDocument = YAML.load('./docs/api-docs.yaml');
+const auth = require('./auth');
 
 // for the checker for hello world
 router.get('/', controller.hello);
 // for the Swagger API Documentation
 router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(apiDocument));
+// auth routes
+router.use('/auth', auth);
 
 module.exports = router;
