a9a4ac42d2d700acce1ee9b5a2aa76855f707981
Junia Uni Umamtun Nysa <unijunia@gmail.com>
2022-11-23 20:52:13 +0700

    db migration

---
+++ db/migrations/20221123101458-create-user.js
@@ -0,0 +1,51 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      url_profile_picture: {
+        type: Sequelize.STRING
+      },
+      gender: {
+        type: Sequelize.ENUM('Male', 'Female')
+      },
+      is_google: {
+        type: Sequelize.BOOLEAN
+      },
+      role: {
+        type: Sequelize.INTEGER
+      },
+      nationality: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
