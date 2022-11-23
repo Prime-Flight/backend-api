65e5b683cfbfc84ef716c436a134acacfd6398bb
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-11-23 21:11:04 +0800

    update api-docs and delete undefined middleware routes

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const router = express.Router(); 
 const controller = require('../controllers');
-const middleware = require('../middlewares');
 
 router.post('/register', controller.auth.register);
 router.post('/login', controller.auth.login);
@@ -11,6 +10,6 @@ router.get('/whoami', controller.auth.google);
 router.get('/forgot-password', controller.auth.viewForgotPassword);
 router.post('/forgot-password', controller.auth.forgotPassword);
 router.post('/reset-password', controller.auth.resetPassword);
-router.post('/verify-user', middleware.restrict, controller.auth.verifyUser);
+router.post('/verify-user', controller.auth.verifyUser);
 
 module.exports = router;
