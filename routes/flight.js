65b0dbb235e7a50f5ad1771192dfe43e1ef3646c
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-12-08 00:40:06 +0800

    [feat] add CRUD for admin flight

--- routes/flight.js
+++ routes/flight.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const controller = require('../controllers');
 const middleware = require('../middlewares')
 
+router.get('/get-airport', middleware.restrict, controller.flight.getAirportData);
 router.get('/get-flight', middleware.restrict, controller.flight.getFlightData);
-// router.post('/create', middleware.restrict, controller.flight.create);
-// router.delete('/delete', middleware.restrict, controller.flight.delete);
+router.post('/create', middleware.restrict, controller.flight.create);
+router.put('/update', middleware.restrict, controller.flight.update);
+router.delete('/delete', middleware.restrict, controller.flight.delete);
 
 module.exports = router;
 
