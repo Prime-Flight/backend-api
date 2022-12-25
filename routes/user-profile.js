247f431722fd89b88c96ca009b57e273f5d3cbfc
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-12-25 19:19:29 +0800

    [feat] update-profile-picture endpoint

--- routes/user-profile.js
+++ routes/user-profile.js
@@ -3,10 +3,13 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers');
 const middleware = require('../middlewares');
+const multer = require('multer');
+const upload = multer();
 
 router.get('/details', middleware.restrict, controller.userProfile.getUserDetails);
 router.put('/update-details', middleware.restrict, controller.userProfile.updateUserDetails);
 router.delete('/delete-details', controller.userProfile.deleteUserDetails);
+router.post('/upload-profile', middleware.restrict, upload.single('media'), controller.userProfile.uploadProfilePicture);
 
 module.exports = router;
 
