ffc753cc7c5fa9cfe37a92a3f0e56b91689e0b3a
danielmanru <85722906+danielmanru@users.noreply.github.com>
2022-12-30 11:16:30 +0700

    Update user-booking.js

--- routes/user-booking.js
+++ routes/user-booking.js
@@ -9,7 +9,7 @@ router.post('/cancelBooking', middleware.restrict, controller.userBooking.cancel
 router.post('/order', middleware.restrict, controller.userBooking.order);
 router.get('/myBooking', middleware.restrict, controller.userBooking.myBooking);
 router.post('/cancel-order', middleware.restrict, controller.userBooking.cancelRequest);
-router.post('/flights', middleware.restrict, controller.userBooking.flights);
+router.get('/flights/search', middleware.restrict, controller.userBooking.flights)
 router.post('/checkout', middleware.restrict, controller.userBooking.checkout);
 router.get('/my-transaction', middleware.restrict, controller.userBooking.getHistory);
 
