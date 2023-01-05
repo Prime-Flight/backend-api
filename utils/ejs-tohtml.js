ccf781d79c54fb1b15bc8ab2a2bd1090c00350e2
Gede Rico Wijaya <gricowijaya@gmail.com>
2023-01-05 16:07:27 +0800

    remove packages to resolve error

--- utils/ejs-tohtml.js
+++ utils/ejs-tohtml.js
@@ -1,18 +1,18 @@
-var fs = require('fs');
-var ejs = require('ejs');
+// var fs = require('fs');
+// var ejs = require('ejs');
 
-module.exports = {
-  toHTML: (ejsTemplateURL, data) => {
-    return new Promise(function(resolve, reject) {
-      fs.readFile(ejsTemplateURL, 'utf8', function(error, response) {
-        if (error) {
-          reject(error);
-        }
-        else {
-          var html = ejs.render(response, data);
-          resolve(html);
-        }
-      });
-    });
-  }
-}
+// module.exports = {
+//   toHTML: (ejsTemplateURL, data) => {
+//     return new Promise(function(resolve, reject) {
+//       fs.readFile(ejsTemplateURL, 'utf8', function(error, response) {
+//         if (error) {
+//           reject(error);
+//         }
+//         else {
+//           var html = ejs.render(response, data);
+//           resolve(html);
+//         }
+//       });
+//     });
+//   }
+// }
--- utils/pdf.js
+++ utils/pdf.js
@@ -1,16 +1,16 @@
-const pdf = require('html-pdf');
+// const pdf = require('html-pdf');
 
-module.exports = {
-  toPDF: async (html, options, output) => {
-    return new Promise(function(resolve, reject) {
-      pdf.create(html, { childProcessOptions: { env: { OPENSSL_CONF: '/dev/null' }}}).toFile(output, function(error, response) {
-        if (error) {
-          reject(error);
-        }
-        else {
-          resolve(response);
-        }
-      });
-    });
-  }
-}
+// module.exports = {
+//   toPDF: async (html, options, output) => {
+//     return new Promise(function(resolve, reject) {
+//       pdf.create(html, { childProcessOptions: { env: { OPENSSL_CONF: '/dev/null' }}}).toFile(output, function(error, response) {
+//         if (error) {
+//           reject(error);
+//         }
+//         else {
+//           resolve(response);
+//         }
+//       });
+//     });
+//   }
+// }
