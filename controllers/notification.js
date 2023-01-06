89d7e51bb77eea2dcc9496adae64f1ed0532b729
Gede Rico Wijaya <gricowijaya@gmail.com>
2023-01-06 15:34:00 +0800

    feat read notification

--- controllers/notification.js
+++ controllers/notification.js
@@ -21,5 +21,27 @@ module.exports = {
       next(err);
     }
   },
+  readNotification: async (req, res, next) => {
+    try {
+      const { id } = req.user;
+      const { notification_id } = req.body;
+      const updateNotification = await Notification.update({ read: true }, { where: { id: notification_id }});
+      const notificationData = await Notification.findOne({ where: { id : notification_id }});
+      if (notificationData.length == 0) {
+        return res.status(204).json({
+          status: true,
+          message: "No Data For Your Notification"
+        });
+      }
+
+      return res.status(200).json({
+        status: true,
+        message: "Successfully Update Notification",
+        data: notificationData
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
 }
 
